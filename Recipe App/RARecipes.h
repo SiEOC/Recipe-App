bb4986c2c555119b88e942feb845cc33ee2c8c2b
Gamaliel Tellez <gamatellez@gmail.com>
2015-04-29 14:36:21 -0600

    Completed to step 4

---
+++ Recipe App/RARecipes.h
@@ -0,0 +1,13 @@
+//
+//  RARecipe.h
+//  Recipe App
+//
+//  Created by Gamaliel Tellez on 4/29/15.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RARecipe : NSObject
+
+@end
---
+++ Recipe App/RARecipes.m
@@ -0,0 +1,13 @@
+//
+//  RARecipe.m
+//  Recipe App
+//
+//  Created by Gamaliel Tellez on 4/29/15.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import "RARecipe.h"
+
+@implementation RARecipe
+
+@end
