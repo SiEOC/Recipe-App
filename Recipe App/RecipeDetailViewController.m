1a6d82daecb4c5ba4e2a4f5f605849141715e975
Gamaliel Tellez <gamatellez@gmail.com>
2015-04-30 16:29:51 -0600

    working on labes for ingredients and types

--- Recipe App/RecipeDetailViewController.m
+++ Recipe App/RecipeDetailViewController.m
@@ -41,15 +41,57 @@ static int topMargin = 20;
     [scrollView addSubview:descriptionLabel];
     
     
+    //creating ingredients label
     
-
-
+    for (int i = 0; i < [RARecipes ingredientCountAtIndex:self.recipeIndex]; i++)
+    {
+        
+        int margin = 20;
+        int yPoint = 140;
+        
+        
+        UILabel *volume = [[UILabel alloc] initWithFrame:CGRectMake(margin, yPoint, (self.view.frame.size.width - 2 * margin) / 4, 20)];
+        volume.font = [UIFont boldSystemFontOfSize:17];
+        volume.text = [RARecipes ingredientVolumeAtIndex:i inRecipeAtIndex:self.recipeIndex];
+        [scrollView addSubview:volume];
+        
+        // The width of the type is just the remaining space. It's possible that this get's truncated, because I'm not wrapping text here.
+        
+        UILabel *type = [[UILabel alloc] initWithFrame:CGRectMake(margin + (self.view.frame.size.width - 2 * margin) / 4, topMargin * 2i, (self.view.frame.size.width - 2 * margin) * 3 / 4, 20)];
+        type.numberOfLines = 0;
+        type.font = [UIFont systemFontOfSize:15];
+        type.text = [RARecipes ingredientTypeAtIndex:i inRecipeAtIndex:self.recipeIndex];
+        [scrollView addSubview:type];
+        
+        // Volume and type are on the same line, so we only update the top margin after they are added to the scrollview
+        
+        topMargin += (20 + margin);
+        
+    }
+   
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+- (CGFloat)heightOfReferenceString:(NSString *)reference {
+    
+    CGRect bounding = [reference boundingRectWithSize:CGSizeMake(self.view.frame.size.width - 2 * margin, 0)
+                                              options:NSStringDrawingUsesLineFragmentOrigin
+                                           attributes:@{NSFontAttributeName:[UIFont systemFontOfSize:15]}
+                                              context:nil];
+    
+    return bounding.size.height;
+    
 }
+
+
+
+
+
+    - (void)didReceiveMemoryWarning {
+        [super didReceiveMemoryWarning];
+        // Dispose of any resources that can be recreated.
+    }
+
 
 /*
 #pragma mark - Navigation
